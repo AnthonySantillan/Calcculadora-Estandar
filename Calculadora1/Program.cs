11870674eea604c20d5842b72c00191186d2323a
AnthonySantillán <fairy.jum568@gmail.com>
2020-12-10 00:26:42 -0500

    calculadora

---
+++ Calculadora1/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Operaciones;
+
+namespace CalculadoraApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\t\t\t\t\t-------Calculadora--------");
+            Console.WriteLine("\t\t\t\t-------Desea realizar alguna operación--------");
+
+            var respuesta = Console.ReadLine();
+
+            while (respuesta != "no")
+            {
+                Console.WriteLine("\t Escoje una operación:" +
+                "\n 1)Suma " +
+                "\n 2)Resta" +
+                "\n 3)Multiplicación " +
+                "\n 4)División " +
+                "\n 5)La inversa de un número " +
+                "\n 6)El cuadrado de un número" +
+                "\n 7)Raíz Cuadrada");
+
+                var seleccion = Console.ReadLine();
+
+                /*Suma*/
+
+                if (seleccion == "1")
+                {
+                    Console.WriteLine("Ingrese el primer número");
+                    var primerNumero1 = Console.ReadLine();
+
+                    Console.WriteLine("Ingrese el segundo número");
+                    var segundoNumero2 = Console.ReadLine();
+
+                    var calculadora = new Logica();
+
+                    var resultado = calculadora.Sumar(int.Parse(primerNumero1), int.Parse(segundoNumero2));
+
+                    Console.WriteLine("La suma es: " + resultado);
+                }
+
+                /*Resta*/
+
+                if (seleccion == "2")
+                {
+                    Console.WriteLine("Ingrese el primer número");
+                    var primerNumero1 = Console.ReadLine();
+
+                    Console.WriteLine("Ingrese el segundo número");
+                    var segundoNumero2 = Console.ReadLine();
+
+                    var calculadora = new Logica();
+
+                    var diferencia = calculadora.Restar(int.Parse(primerNumero1), int.Parse(segundoNumero2));
+
+                    Console.WriteLine("La resta es: " + diferencia);
+
+                }
+
+                /*Multiplicacion*/
+
+                if (seleccion == "3")
+                {
+                    Console.WriteLine("Ingrese el primer número");
+                    var primerNumero1 = Console.ReadLine();
+
+                    Console.WriteLine("Ingrese el segundo número");
+                    var segundoNumero2 = Console.ReadLine();
+
+                    var calculadora = new Logica();
+
+                    var resultado = calculadora.Multiplicar(int.Parse(primerNumero1), int.Parse(segundoNumero2));
+
+                    Console.WriteLine("El multiplicación da como resultado: " + resultado);
+
+
+                }
+
+                /*Division*/
+
+                if (seleccion == "4")
+                {
+                    Console.WriteLine("Ingrese el primer número");
+                    var primerNumero1 = Console.ReadLine();
+
+                    Console.WriteLine("Ingrese el segundo número");
+                    var segundoNumero2 = Console.ReadLine();
+
+                    var calculadora = new Logica();
+
+                    var resultado = calculadora.Dividir(int.Parse(primerNumero1), int.Parse(segundoNumero2));
+
+                    Console.WriteLine("La división da como resultado: " + resultado);
+                }
+
+                /*Operacion inversa*/
+
+                if (seleccion == "5")
+                {
+                    Console.WriteLine("Ingresa el numero que quieres ver su inversa");
+
+                    var numero = Console.ReadLine();
+
+                    var calculadora = new Logica();
+
+                    var resultado = calculadora.Inversa(double.Parse(numero));
+
+                    Console.WriteLine("La inversa del número  " + numero + " es:  " + resultado);
+                }
+
+                /*Elevacion al cuadrado de un numero */
+
+                if (seleccion == "6")
+                {
+                    Console.WriteLine("Ingrese el número que quiere ver su cuadrado");
+
+                    var numero = Console.ReadLine();
+
+                    var calculadora = new Logica();
+
+                    var resultado = calculadora.Cuadrado(int.Parse(numero));
+
+                    Console.WriteLine("El cuadrado del " + numero + " es " + resultado);
+
+                }
+
+                /*Raiz cuadrada*/
+
+                if (seleccion == "7")
+                {
+                    Console.WriteLine("Ingrese el número al que desea obtener su raíz cuadrada");
+
+                    var numero = Console.ReadLine();
+
+                    var calculadora = new Logica();
+
+                    var resultado = calculadora.Raiz(double.Parse(numero));
+
+                    Console.WriteLine("La raíz del número " + numero + " es " + resultado);
+                }
+
+
+                Console.WriteLine("Desea realizar otra operación.?");
+                respuesta = Console.ReadLine();
+                Console.Clear();
+            }
+
+
+
+        }
+    }
+}
---
+++ Operaciones/Operaciones.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Operaciones
+{
+    public class Logica
+    {
+        /*Suma*/
+        public int Sumar(int sumando1, int sumando2)
+        {
+            return sumando1 + sumando2;
+        }
+
+        /*Resta*/
+        public int Restar(int minuendo, int sustraendo)
+        {
+            return minuendo - sustraendo;
+        }
+
+        /*Multiplicacion*/
+        public int Multiplicar(int factor1, int factor2)
+        {
+            return factor1 * factor2;
+        }
+
+        /*Division*/
+        public int Dividir(int dividendo, int divisor)
+        {
+            return dividendo / divisor;
+        }
+
+        /*Operacion inversa*/
+        public double Inversa( double numero)
+        {
+            return  1 / numero;
+        }
+
+        /*Elevacion al cuadrado de un numero */
+        public int Cuadrado( int numero)
+        {
+
+            return numero * numero;
+        }
+
+        /*Raiz cuadrada*/
+        public double Raiz ( double numero)
+        {
+            return Math.Sqrt(numero);
+
+            
+            
+        }
+    }
+}
